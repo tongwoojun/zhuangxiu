3fbee41f837e8d796bb69144ad690b2c59de3129
tongwoojun <tongwoojun@gmail.com>
2016-01-11 13:40:56 +0800

    修改活动
    修改活动

--- frontend/controllers/ActionController.php
+++ frontend/controllers/ActionController.php
@@ -4,10 +4,32 @@ namespace frontend\controllers;
 
 use common\con\FrontendController;
 
+use Yii;
+use common\models\Form;
+
 class ActionController extends FrontendController{
 
     public function actionIndex(){
         return $this->render('index');
+    }
+
+
+    public function actionGetform(){
+        $model = new Form();
+        if ($model->load(Yii::$app->request->get())) {
+            $model->ip = Yii::$app->request->userIP;
+            if ($model->save()) {
+                return $this->toJson(['status' => 1, 'info' => '提交成功']);
+            }else{
+                $error = $model->getErrors();
+                if(!$error){
+                    return $this->toJson(['status' => -1, 'info' => '未知错误']);
+                }
+                foreach($error as $value){
+                    return $this->toJson(['status' => 0, 'info' => $value[0]]);
+                }
+            }
+        }
     }
 
 }
