742d7dee668ebbb0cd00ff701e90dc937fab5f7e
tongwoojun <tongwoojun@gmail.com>
2016-01-17 23:35:33 +0800

    后台配置文件修改
    后台配置文件修改

--- common/config/params.php
+++ common/config/params.php
@@ -4,6 +4,6 @@ return [
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
     'uploadDir'=>dirname(dirname(__DIR__)) . '/backend/web/',
-    'imgurl'=>'http://www.homerenew.com.cn／',
-    'siteurl'=>'http://www.homerenew.com.cn/frontend/web/',
+    'imgurl'=>'http://www.homerenew.com.cn/',
+    'siteurl'=>'http://www.homerenew.com.cn/',
 ];
