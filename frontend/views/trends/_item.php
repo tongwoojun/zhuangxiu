a22771027e416c6586852be9089e186f5e66295e
tongwoojun <tongwoojun@gmail.com>
2016-02-03 22:55:24 +0800

    修改链接
    修改链接

--- frontend/views/trends/_item.php
+++ frontend/views/trends/_item.php
@@ -11,9 +11,9 @@ use yii\helpers\Html;
 use yii\helpers\StringHelper;
 ?>
 <dl>
-    <dt><img src="<?=Url::to([$model->img]);?>" width="220"></dt>
+    <dt><a href="<?=Url::to(['trends/detail','id'=>$model->id]);?>" ><img src="<?=Url::to([$model->img]);?>" width="220"></a></dt>
     <dd>
-        <h2><?=Html::encode($model->title);?></h2>
+        <h2><a href="<?=Url::to(['trends/detail','id'=>$model->id]);?>" ><?=Html::encode($model->title);?></a></h2>
         <p><?=StringHelper::truncate($model->desc,35);?> <a href="<?=Url::to(['trends/detail','id'=>$model->id]);?>" class="f_e7340c">[详情]</a></p>
         <p><?=$model->time;?></p>
     </dd>
